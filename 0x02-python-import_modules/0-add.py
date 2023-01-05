c424b2c740cef152fc7ce8640610668c108bcf87
Nonso024 <josephani772@gmail.com>
2023-01-05 11:02:09 -0800

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import add from add_0
+
+    a = 2
+    b = 3
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Readme file for Solutiom on tasks on module importation in python
