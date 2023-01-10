f5035e7bc00a2b5f00cb2a73c4d04c9b6a12bf0b
Nonso024 <josephani772@gmail.com>
2023-01-10 02:41:17 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence = '':
-        first_char = None
+    if sentence != '':
+        first_char =sentence[0]
     else:
-        return(len(sentence), first_char)
+        first_char = None
+        return (len(sentence), first_char)
