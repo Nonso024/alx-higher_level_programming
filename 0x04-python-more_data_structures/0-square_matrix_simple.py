b6518626a0064e24383fa8c7015cdd124e7bd8e1
Nonso024 <josephani772@gmail.com>
2023-01-11 13:00:14 -0800

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+    if len(matrix) == 0:
+        return None
+    else:
+        matrix_sq = [[x ** 2 for x in row] for row in matrix]
+        return matrix_sq
