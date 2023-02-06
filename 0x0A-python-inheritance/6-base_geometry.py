4fbd6b652616596ef3401fa9ef13fa5ad9fa1105
Nonso024 <josephani772@gmail.com>
2023-02-06 14:32:25 -0800

    mandatory

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module conatains a class BaseGeometry and a public instance method area()
+    that raises an exception
+"""
+
+
+class BaseGeometry:
+    """ Class that contains public instance method area() """
+
+    def area(self):
+        """ method that raises an exception """
+
+        raise Exception("area() is not implemented")
