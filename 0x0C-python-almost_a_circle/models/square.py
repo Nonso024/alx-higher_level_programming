c2675ae4664de2798e16e531f6e1de172aa151a6
Nonso024 <josephani772@gmail.com>
2023-02-13 09:57:30 -0800

    mandatory fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-""" This module contains a representation of a square """
+"""
+    This module contains a square class inherited from the rectangle
+    class
+"""
+
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ A class that represents a square """
-    def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes the attributes of a square """
-        self.size = size
-        self.x = x
-        self.y = y
-        self.id = None
-        super()__init__(size, x, y, id)
+    """ A square class inherited from a rectangle class """
 
-    def __str__(self):
-        """ Defines a format for string representation of the class """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-    
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initializes self """
+
+        super().__init__(size, size, x, y, id)
+
     @property
     def size(self):
-        """ Gets the value of size """
-        return self.__width
+        """ returns size """
+
+        return self.width
 
     @size.setter
-    """ sets size """
     def size(self, value):
-        """ Sets the value for size """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """ sets size to value """
 
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ calls when instance is printed """
+
+        s = "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
+
+        return s
 
     def update(self, *args, **kwargs):
         """ updates attributes of the instance """
@@ -41,14 +43,13 @@ class Square(Rectangle):
         attrs = ["id", "size", "x", "y"]
 
         if not args:
-            for key, value in kwargs.items():
+            for key, val in kwargs.items():
                 if key in attrs:
-                    setattr(self, key, value)
-
+                    setattr(self, key, val)
         else:
             i = 0
             for val in args:
-                setattr(self, attrs[i], value)
+                setattr(self, attrs[i], val)
                 i += 1
                 if i > 3:
                     break
@@ -57,10 +58,10 @@ class Square(Rectangle):
         """ returns the dictionary representation """
 
         attrs = ["id", "size", "x", "y"]
-        dict_rep = {}
+        dict_r = {}
 
         for attr in attrs:
             val = getattr(self, attr)
-            dict_rep[attr] = val
+            dict_r[attr] = val
 
-        return dict_rep
+        return dict_r
