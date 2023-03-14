81055b2bb21f147f345e90cfcfffb30490e96671
Nonso024 <josephani772@gmail.com>
2023-03-14 07:42:33 -0700

    all mandatory

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,5 @@
 -- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- all fields should be printed
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
 
-SELECT * FROM hbtn_0c_0.first_table;
+SELECT * FROM first_table;
