c26bf029f8a15966c31127253d947a33c10d3fbf
Nonso024 <josephani772@gmail.com>
2023-04-11 23:54:58 +0100

    tasks done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const num = Number(args[0]);
 if (Number.isNaN(num)) {
-  console.log('Missing number of occurences');
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) console.log('C is fun');
+  for (let i = 0; i < num; i++) console.log('X'.repeat(num));
 }
