dd3671b0928017b6993bdcd9b24a4beff5de7670
Nonso024 <josephani772@gmail.com>
2023-04-13 20:32:40 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const item of list) {
     if (item === searchElement) count++;
