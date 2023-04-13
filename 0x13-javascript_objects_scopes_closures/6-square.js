4a1c9207853d98d29acebb640f4959d99420ac60
Nonso024 <josephani772@gmail.com>
2023-04-13 20:30:06 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const PrevSquare = require('./5.square');
+const PrevSquare = require('./5-square');
 module.exports = class Square extends PrevSquare {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
