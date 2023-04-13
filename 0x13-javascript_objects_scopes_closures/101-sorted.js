a4bdaf364ce34287b6386c5f284499fdb67d3a3e
Nonso024 <josephani772@gmail.com>
2023-04-13 20:26:57 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,9 +3,9 @@ const { dict } = require('./101-data');
 const newDict = {};
 
 Object.keys(dict).map((key, index) => {
-	if (newDict[dict[key]] === undefined) newDict[dict[key]] = [];
-	newDict[dict[key]].push(key);
-	return index;
+  if (newDict[dict[key]] === undefined) newDict[dict[key]] = [];
+  newDict[dict[key]].push(key);
+  return index;
 });
 
 console.log(newDict);
