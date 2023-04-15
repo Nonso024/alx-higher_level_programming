99874ad3d774a8d091f32fc553627b185db9cf93
Nonso024 <josephani772@gmail.com>
2023-04-15 20:19:23 +0100

    tasks done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and
-displays all values in the states where
-`name` matches the arg from the database.
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb as db
 from sys import argv
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     """
-    Enables access to datababse and
-    all neccesary information.
+    Access to the database and get the states
+    from the database.
     """
     db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], psswd=argv[2], db=argv[3])
+                            user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                                            states.id ASC".format(argv[4]))
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
