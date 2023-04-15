eb241b45c2994bc5952cb462ec5783a36aef5d4f
Nonso024 <josephani772@gmail.com>
2023-04-15 20:12:19 +0100

    task done

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script prints the first State object
+This script changes the name of a State object
 from the database `hbtn_0e_6_usa`.
 """
 
@@ -11,8 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database and get a state
-    from the database.
+    Updates a State object on the database.
     """
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
@@ -23,8 +22,8 @@ if __name__ == "__main__":
 
     session = Session()
 
-    state = session.query(State).filter(State.name == argv[4]).first()
-    if state is not None:
-        print('{0}'.format(state.id))
-    else:
-        print("Not found")
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+
+    session.close()
